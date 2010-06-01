b7573d4222ed0e18ee56de9b540657219ad0b20f
Mikkel Hoegh <mikkel@hoegh.org>
2010-06-02 00:22:56 +0200

    Starting password_required app.

---
+++ password_required/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Mikkel Høgh'
+__version__ = (0, 1, 0)
+
---
+++ password_required/models.py
@@ -0,0 +1,3 @@
+""" The mandatory models.py to make Django consider this to be an app. """
+from django.db import models
+
